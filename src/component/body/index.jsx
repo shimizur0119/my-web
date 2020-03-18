445a78bac4db950ff1af4142f70b8c5ed1445dfe
shimizur0119 <otsukaresamannsatabasa@gmail.com>
2020-03-18 23:28:07 +0900

    fix-bugs

--- src/component/body/index.jsx
+++ src/component/body/index.jsx
@@ -20,6 +20,7 @@ const Body = () => {
     <Box m={3}>
       <Route path="/" render={()=><Redirect to="/my-web/home" />} />
       <Route path="/my-web" render={()=><Redirect to="/my-web/home" />} />
+      <Route path="/my-web/home" render={()=><Redirect to="/my-web/home" />} />
       <Route path="/my-web/home" component={Home} />
       <Route path="/my-web/profile" component={Profile} />
     </Box>
